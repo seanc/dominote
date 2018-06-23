686a2889c42fae6e83882644a57cd3577327c975
Sean Wilson <hello@imsean.me>
2018-06-23 10:04:52 -0500

    Update user migration with needed columns, Change default driver to mysql

--- config/database.js
+++ config/database.js
@@ -13,7 +13,7 @@ module.exports = {
   | interacting with SQL databases.
   |
   */
-  connection: Env.get('DB_CONNECTION', 'sqlite'),
+  connection: Env.get('DB_CONNECTION', 'mysql'),
 
   /*
   |--------------------------------------------------------------------------
@@ -51,7 +51,7 @@ module.exports = {
       port: Env.get('DB_PORT', ''),
       user: Env.get('DB_USER', 'root'),
       password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      database: Env.get('DB_DATABASE', 'dominote')
     }
   },
 
--- database/migrations/1503248427885_user.js
+++ database/migrations/1503248427885_user.js
@@ -6,8 +6,10 @@ class UserSchema extends Schema {
   up () {
     this.create('users', (table) => {
       table.increments()
+      table.integer('employee_id').notNullable().unique().index()      
       table.string('username', 80).notNullable().unique()
-      table.string('email', 254).notNullable().unique()
+      table.string('first_name').notNullable()
+      table.string('last_name').notNullable()
       table.string('password', 60).notNullable()
       table.timestamps()
     })
