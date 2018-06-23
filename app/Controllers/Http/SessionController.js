033bbfd44d5fad7a883aebab757b8b5002e440b9
Sean Wilson <hello@imsean.me>
2018-06-23 11:55:19 -0500

    Finish store method in session controller

--- app/Controllers/Http/SessionController.js
+++ app/Controllers/Http/SessionController.js
@@ -2,10 +2,28 @@
 
 class SessionController {
 
-  // TODO: store, delete
+  // TODO: delete
 
   create ({ view }) {
     return view.render('session.create')
+  }
+
+  async store ({ auth, request, response, session }) {
+    const { username, password, remember_me = false } = request.all()
+
+    try {
+      await auth
+        .remember(!!remember_me)
+        .attempt(username, password)
+    } catch (e) {
+      session
+        .flashExcept(['password'])
+        .flash({ error: 'Incorrect username or password' })
+
+      return response.redirect('back')
+    }
+
+    return response.redirect('/')
   }
 
 }
