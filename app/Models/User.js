3103a1214b8f0003cdd2d336ae225003c51854b2
Sean Wilson <hello@imsean.me>
2018-06-28 00:13:37 -0500

    Move computed fullname property to customer model

--- app/Models/User.js
+++ app/Models/User.js
@@ -18,14 +18,6 @@ class User extends Model {
     })
   }
 
-  static get computed () {
-    return ['fullname']
-  }
-
-  getFullname ({ firstname, lastname }) {
-    return `${firstname} ${lastname}`
-  }
-
   /**
    * A relationship on tokens is required for auth to
    * work. Since features like `refreshTokens` or
