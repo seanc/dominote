bec0c878f59436b0ef0cd855c60d4a47da31e444
Sean Wilson <hello@imsean.me>
2018-06-28 01:37:21 -0500

    Add origin id and origin to google maps url

--- app/Models/Hooks/CustomerHook.js
+++ app/Models/Hooks/CustomerHook.js
@@ -2,10 +2,15 @@
 
 const CustomerHook = exports = module.exports = {}
 
+const Env = use('Env')
+
 CustomerHook.computeMapsUrl = (customers) => {
   return customers.map(customer => {
     const baseDirectionUrl = 'https://www.google.com/maps/dir/?api=1'
-    const addressMapsUrl = `${baseDirectionUrl}&destination=${encodeURI(customer.address)}`
+    const originId = Env.get('MAPS_ORIGIN_ID', 'ChIJA5mrT5qbskwRDMzvDCpo79s')
+    const origin = encodeURI(Env.get('MAPS_ORIGIN'))
+
+    const addressMapsUrl = `${baseDirectionUrl}&origin_id=${originId}&origin=${origin}&destination=${encodeURI(customer.address)}`
   
     customer.addressMapsUrl = addressMapsUrl
   })
